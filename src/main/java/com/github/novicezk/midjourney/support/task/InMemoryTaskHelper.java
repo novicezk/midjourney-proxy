25d920391a8322161c988757d7d484a599d4cd0d
jiangying <krivergo3@gmail.com>
2023-05-08 14:34:24 +0800

    optimize bean creation

--- src/main/java/com/github/novicezk/midjourney/support/task/InMemoryTaskHelper.java
+++ src/main/java/com/github/novicezk/midjourney/support/task/InMemoryTaskHelper.java
@@ -5,18 +5,19 @@ import cn.hutool.cache.impl.TimedCache;
 import cn.hutool.core.collection.ListUtil;
 import cn.hutool.core.stream.StreamUtil;
 import com.github.novicezk.midjourney.ProxyProperties;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Iterator;
 import java.util.List;
 
 @Component
-@RequiredArgsConstructor
 public class InMemoryTaskHelper implements TaskHelper {
-	private final ProxyProperties properties;
 	// 创建缓存
-	private final TimedCache<String, Task> TASK_MAP = CacheUtil.newTimedCache(properties.getTaskStore().getTimeout());
+	private final TimedCache<String, Task> TASK_MAP;
+
+	public InMemoryTaskHelper(ProxyProperties properties) {
+		TASK_MAP = CacheUtil.newTimedCache(properties.getTaskStore().getTimeout());
+	}
 
 	public void putTask(String key, Task task) {
 		TASK_MAP.put(key, task);
