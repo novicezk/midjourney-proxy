3155b757066b2cd5749b715a130980b3dad3ffbc
zhukai <zhukai@homolo.com>
2023-05-10 22:07:23 +0800

    feat: 添加describe功能

---
+++ src/main/java/com/github/novicezk/midjourney/dto/DescribeDTO.java
@@ -0,0 +1,23 @@
+package com.github.novicezk.midjourney.dto;
+
+import lombok.Data;
+
+@Data
+public class DescribeDTO {
+	/**
+	 * 自定义参数.
+	 */
+	private String state;
+	/**
+	 * 文件名称.
+	 */
+	private String fileName;
+	/**
+	 * 文件base64: data:image/png;base64,xxx.
+	 */
+	private String base64;
+	/**
+	 * notifyHook of caller.
+	 */
+	private String notifyHook;
+}
--- src/main/java/com/github/novicezk/midjourney/service/DiscordServiceImpl.java
+++ src/main/java/com/github/novicezk/midjourney/service/DiscordServiceImpl.java
@@ -5,35 +5,45 @@ import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.text.CharSequenceUtil;
 import com.github.novicezk.midjourney.ProxyProperties;
 import com.github.novicezk.midjourney.result.Message;
+import eu.maxschuster.dataurl.DataUrl;
+import eu.maxschuster.dataurl.DataUrlSerializer;
+import eu.maxschuster.dataurl.IDataUrlSerializer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.net.MalformedURLException;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class DiscordServiceImpl implements DiscordService {
 	private final ProxyProperties properties;
-
 	private static final String DISCORD_API_URL = "https://discord.com/api/v9/interactions";
-	private static final String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36";
+	private String userAgent;
+
+	private String discordUploadUrl;
 
 	private String imagineParamsJson;
 	private String upscaleParamsJson;
 	private String variationParamsJson;
 	private String resetParamsJson;
+	private String describeParamsJson;
 
 	private String discordUserToken;
 	private String discordGuildId;
@@ -44,11 +54,14 @@ public class DiscordServiceImpl implements DiscordService {
 		this.discordUserToken = this.properties.getDiscord().getUserToken();
 		this.discordGuildId = this.properties.getDiscord().getGuildId();
 		this.discordChannelId = this.properties.getDiscord().getChannelId();
+		this.discordUploadUrl = "https://discord.com/api/v9/channels/" + this.discordChannelId + "/attachments";
+		this.userAgent = this.properties.getDiscord().getUserAgent();
 		try {
 			this.imagineParamsJson = IoUtil.readUtf8(ResourceUtils.getURL("classpath:api-params/imagine.json").openStream());
 			this.upscaleParamsJson = IoUtil.readUtf8(ResourceUtils.getURL("classpath:api-params/upscale.json").openStream());
 			this.variationParamsJson = IoUtil.readUtf8(ResourceUtils.getURL("classpath:api-params/variation.json").openStream());
 			this.resetParamsJson = IoUtil.readUtf8(ResourceUtils.getURL("classpath:api-params/reset.json").openStream());
+			this.describeParamsJson = IoUtil.readUtf8(ResourceUtils.getURL("classpath:api-params/describe.json").openStream());
 		} catch (IOException e) {
 			// can't happen
 		}
@@ -61,7 +74,7 @@ public class DiscordServiceImpl implements DiscordService {
 		JSONObject params = new JSONObject(paramsStr);
 		params.getJSONObject("data").getJSONArray("options").getJSONObject(0)
 				.put("value", prompt);
-		return postJson(params.toString());
+		return postJsonAndCheckStatus(params.toString());
 	}
 
 	@Override
@@ -71,7 +84,7 @@ public class DiscordServiceImpl implements DiscordService {
 				.replace("$message_id", messageId)
 				.replace("$index", String.valueOf(index))
 				.replace("$message_hash", messageHash);
-		return postJson(paramsStr);
+		return postJsonAndCheckStatus(paramsStr);
 	}
 
 	@Override
@@ -81,7 +94,7 @@ public class DiscordServiceImpl implements DiscordService {
 				.replace("$message_id", messageId)
 				.replace("$index", String.valueOf(index))
 				.replace("$message_hash", messageHash);
-		return postJson(paramsStr);
+		return postJsonAndCheckStatus(paramsStr);
 	}
 
 	@Override
@@ -90,17 +103,87 @@ public class DiscordServiceImpl implements DiscordService {
 				.replace("$channel_id", this.discordChannelId)
 				.replace("$message_id", messageId)
 				.replace("$message_hash", messageHash);
-		return postJson(paramsStr);
+		return postJsonAndCheckStatus(paramsStr);
 	}
 
-	private Message<Void> postJson(String paramsStr) {
+	@Override
+	public Message<String> upload(String fileName, String imageBase64) {
+		IDataUrlSerializer serializer = new DataUrlSerializer();
+		try {
+			DataUrl dataUrl = serializer.unserialize(imageBase64);
+			JSONObject fileObj = new JSONObject();
+			fileObj.put("filename", fileName);
+			fileObj.put("file_size", dataUrl.getData().length);
+			fileObj.put("id", "0");
+			JSONObject params = new JSONObject()
+					.put("files", new JSONArray().put(fileObj));
+			ResponseEntity<String> responseEntity = postJson(this.discordUploadUrl, params.toString());
+			if (responseEntity.getStatusCode() != HttpStatus.OK) {
+				log.error("上传图片到discord失败, status: {}, msg: {}", responseEntity.getStatusCodeValue(), responseEntity.getBody());
+				return Message.of(Message.VALIDATION_ERROR_CODE, "上传图片到discord失败");
+			}
+			JSONArray array = new JSONObject(responseEntity.getBody()).getJSONArray("attachments");
+			if (array.length() == 0) {
+				return Message.of(Message.VALIDATION_ERROR_CODE, "上传图片到discord失败");
+			}
+			String uploadUrl = array.getJSONObject(0).getString("upload_url");
+			String uploadFilename = array.getJSONObject(0).getString("upload_filename");
+			putFile(uploadUrl, fileName, dataUrl);
+			return Message.success(uploadFilename);
+		} catch (MalformedURLException e) {
+			return Message.of(Message.VALIDATION_ERROR_CODE, "base64格式错误");
+		} catch (Exception e) {
+			log.error("上传图片到discord失败", e);
+			return Message.of(Message.FAILURE_CODE, "上传图片到discord失败");
+		}
+	}
+
+	@Override
+	public Message<Void> describe(String finalFileName) {
+		String fileName = CharSequenceUtil.subAfter(finalFileName, "/", true);
+		String paramsStr = this.describeParamsJson.replace("$guild_id", this.discordGuildId)
+				.replace("$channel_id", this.discordChannelId)
+				.replace("$file_name", fileName)
+				.replace("$final_file_name", finalFileName);
+		return postJsonAndCheckStatus(paramsStr);
+	}
+
+	private void putFile(String uploadUrl, String fileName, DataUrl dataUrl) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("User-Agent", this.userAgent);
+		MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+		ByteArrayResource fileAsResource = new ByteArrayResource(dataUrl.getData()) {
+			@Override
+			public String getFilename() {
+				return fileName;
+			}
+
+			@Override
+			public long contentLength() {
+				return dataUrl.getData().length;
+			}
+		};
+		params.add("file", fileAsResource);
+		HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(params, headers);
+		new RestTemplate().put(uploadUrl, requestEntity);
+	}
+
+	private ResponseEntity<String> postJson(String paramsStr) {
+		return postJson(DISCORD_API_URL, paramsStr);
+	}
+
+	private ResponseEntity<String> postJson(String url, String paramsStr) {
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		headers.set("Authorization", this.discordUserToken);
-		headers.add("user-agent", USER_AGENT);
+		headers.add("User-Agent", this.userAgent);
 		HttpEntity<String> httpEntity = new HttpEntity<>(paramsStr, headers);
+		return new RestTemplate().postForEntity(url, httpEntity, String.class);
+	}
+
+	private Message<Void> postJsonAndCheckStatus(String paramsStr) {
 		try {
-			ResponseEntity<String> responseEntity = new RestTemplate().postForEntity(DISCORD_API_URL, httpEntity, String.class);
+			ResponseEntity<String> responseEntity = postJson(paramsStr);
 			if (responseEntity.getStatusCode() == HttpStatus.NO_CONTENT) {
 				return Message.success();
 			}
