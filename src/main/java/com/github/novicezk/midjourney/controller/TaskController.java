cf4f36ceda57121ec2282cf518e1b223bb47e8df
Lcry <i@51it.wang>
2023-05-16 22:47:54 +0800

    feat(*): 集成Knife4j方便自测，格式化.editorconfig，升级项目到1.5.2

--- src/main/java/com/github/novicezk/midjourney/controller/TaskController.java
+++ src/main/java/com/github/novicezk/midjourney/controller/TaskController.java
@@ -2,6 +2,9 @@ package com.github.novicezk.midjourney.controller;
 
 import com.github.novicezk.midjourney.service.TaskService;
 import com.github.novicezk.midjourney.support.Task;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,19 +13,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(tags = "任务模块")
 @RestController
 @RequestMapping("/task")
 @RequiredArgsConstructor
 public class TaskController {
 	private final TaskService taskService;
 
+	@ApiOperation(value = "列出所有任务信息")
 	@GetMapping("/list")
 	public List<Task> listTask() {
 		return this.taskService.listTask();
 	}
 
+	@ApiOperation(value = "列出指定id任务信息")
 	@GetMapping("/{id}/fetch")
-	public Task getTask(@PathVariable String id) {
+	public Task getTask(@ApiParam(value = "任务id") @PathVariable String id) {
 		return this.taskService.getTask(id);
 	}
 
