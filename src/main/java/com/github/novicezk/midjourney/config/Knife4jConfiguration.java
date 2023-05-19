1cd93d28b0aaf3b41241919522729203253dce97
zhukai <zhukai@homolo.com>
2023-05-19 14:43:02 +0800

    fix: 更新二维码

--- src/main/java/com/github/novicezk/midjourney/config/Knife4jConfiguration.java
+++ src/main/java/com/github/novicezk/midjourney/config/Knife4jConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
@@ -13,21 +14,19 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 @EnableSwagger2WebMvc
 public class Knife4jConfiguration {
 
-	@Bean(value = "defaultApi2")
+	@Bean
 	public Docket defaultApi2() {
-		Docket docket = new Docket(DocumentationType.SWAGGER_2)
+		return new Docket(DocumentationType.SWAGGER_2)
 				.apiInfo(new ApiInfoBuilder()
-						.title("Midjourney Proxy RESTful APIs")
-						.description("#Midjourney Proxy RESTful APIs")
+						.contact(new Contact("novicezk", "https://github.com/novicezk", "zhukai_novice@163.com"))
+						.title("Midjourney Proxy API文档")
+						.description("提交Midjourney任务，接收任务结果")
 						.termsOfServiceUrl("https://github.com/novicezk/midjourney-proxy")
-						.version("1.0")
+						.version("1.x")
 						.build())
-				//分组名称
 				.select()
-				//这里指定Controller扫描包路径
 				.apis(RequestHandlerSelectors.basePackage("com.github.novicezk.midjourney.controller"))
 				.paths(PathSelectors.any())
 				.build();
-		return docket;
 	}
 }
