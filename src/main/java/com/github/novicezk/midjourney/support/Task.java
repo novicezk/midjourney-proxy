907732a1c4f771e7b55ef5408e8895b9097b23fd
zhukai <zhukai@homolo.com>
2023-05-23 12:33:54 +0800

    doc: 完善api文档

--- src/main/java/com/github/novicezk/midjourney/support/Task.java
+++ src/main/java/com/github/novicezk/midjourney/support/Task.java
@@ -3,28 +3,41 @@ package com.github.novicezk.midjourney.support;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.github.novicezk.midjourney.enums.Action;
 import com.github.novicezk.midjourney.enums.TaskStatus;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serial;
 import java.io.Serializable;
 
 @Data
+@ApiModel("任务")
 public class Task implements Serializable {
 	@Serial
 	private static final long serialVersionUID = -674915748204390789L;
 
 	private Action action;
+	@ApiModelProperty("任务ID")
 	private String id;
+	@ApiModelProperty("提示词")
 	private String prompt;
+	@ApiModelProperty("提示词-英文")
 	private String promptEn;
 
+	@ApiModelProperty("任务描述")
 	private String description;
+	@ApiModelProperty("自定义参数")
 	private String state;
+	@ApiModelProperty("提交时间")
 	private Long submitTime;
+	@ApiModelProperty("开始执行时间")
 	private Long startTime;
+	@ApiModelProperty("结束时间")
 	private Long finishTime;
+	@ApiModelProperty("图片url")
 	private String imageUrl;
 	private TaskStatus status = TaskStatus.NOT_START;
+	@ApiModelProperty("失败原因")
 	private String failReason;
 
 	// Hidden -- start
