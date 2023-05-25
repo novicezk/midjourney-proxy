c04b32d20bda3376d4f465507ef8d3c2392a7818
wchen <wu.chen@dahiti.com>
2023-05-25 15:46:53 +0800

    test

--- src/main/java/com/github/novicezk/midjourney/ProxyApplication.java
+++ src/main/java/com/github/novicezk/midjourney/ProxyApplication.java
@@ -11,6 +11,10 @@ import spring.config.BeanConfig;
 @Import(BeanConfig.class)
 public class ProxyApplication {
 
+	/**
+	 * 测试
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyApplication.class, args);
 	}
