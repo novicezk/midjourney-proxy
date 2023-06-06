f9a3641e3fd60abc34b16609320072c4fa412d80
zhukai <zhukai@homolo.com>
2023-06-06 10:06:02 +0800

    fix: 优化docker镜像

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -13,7 +13,17 @@ if [ $# -ge 2 ]; then
   ARCH=$2
 fi
 
+JAR_FILE_COUNT=$(find "../target/" -maxdepth 1 -name '*.jar' | wc -l)
+if [ $JAR_FILE_COUNT == 0  ]; then
+    echo "jar file not found, please execute: mvn clean package"
+    exit 1
+fi
+
+cp ../target/*.jar ./app.jar
+ls -l ./app.jar
+
 docker build . -t midjourney-proxy:${VERSION}
+rm -rf ./app.jar
 
 docker tag midjourney-proxy:${VERSION} novicezk/midjourney-proxy-${ARCH}:${VERSION}
 docker push novicezk/midjourney-proxy-${ARCH}:${VERSION}
