4e080f408e1c48cb06a9072bccf0ef83e4c3cdb6
zhukai <zhukai@homolo.com>
2023-06-06 09:24:51 +0800

    feat: 升级2.1.5

--- src/main/java/com/github/novicezk/midjourney/controller/TaskController.java
+++ src/main/java/com/github/novicezk/midjourney/controller/TaskController.java
@@ -1,6 +1,7 @@
 package com.github.novicezk.midjourney.controller;
 
 import cn.hutool.core.comparator.CompareUtil;
+import com.github.novicezk.midjourney.dto.TaskConditionDTO;
 import com.github.novicezk.midjourney.service.TaskStoreService;
 import com.github.novicezk.midjourney.support.Task;
 import com.github.novicezk.midjourney.support.TaskQueueHelper;
@@ -10,9 +11,12 @@ import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
@@ -47,6 +51,15 @@ public class TaskController {
 		return queueTaskIds.stream().map(this.taskStoreService::get).filter(Objects::nonNull)
 				.sorted(Comparator.comparing(Task::getSubmitTime))
 				.toList();
+	}
+
+	@ApiOperation(value = "根据条件查询任务")
+	@PostMapping("/list-by-condition")
+	public List<Task> listByCondition(@RequestBody TaskConditionDTO conditionDTO) {
+		if (conditionDTO.getIds() == null) {
+			return Collections.emptyList();
+		}
+		return conditionDTO.getIds().stream().map(this.taskStoreService::get).filter(Objects::nonNull).toList();
 	}
 
 }
