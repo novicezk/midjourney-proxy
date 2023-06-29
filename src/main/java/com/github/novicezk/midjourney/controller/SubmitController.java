4e41632b872602f83ab3813a13c7556a52ecdd4a
Lafrenze <39109490+Lafreze@users.noreply.github.com>
2023-06-29 11:06:56 +0900

    replace “

--- src/main/java/com/github/novicezk/midjourney/controller/SubmitController.java
+++ src/main/java/com/github/novicezk/midjourney/controller/SubmitController.java
@@ -56,8 +56,6 @@ public class SubmitController {
 		if (CharSequenceUtil.isBlank(prompt)) {
 			return SubmitResultVO.fail(ReturnCode.VALIDATION_ERROR, "prompt不能为空");
 		}
-		prompt = prompt.replace("\"", "");
-		System.out.println(prompt);
 		prompt = prompt.trim();
 		Task task = newTask(imagineDTO);
 		task.setAction(TaskAction.IMAGINE);
@@ -85,7 +83,7 @@ public class SubmitController {
 			}
 		}
 		task.setPromptEn(promptEn);
-		task.setDescription("/imagine " + prompt);
+		task.setDescription("/imagine " + prompt.replace("\"", ""));
 		return this.taskService.submitImagine(task, dataUrl);
 	}
 
