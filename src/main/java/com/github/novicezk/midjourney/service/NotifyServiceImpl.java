3289211164ddb318f0efeeb66c4702ca806ccb06
zhukai <zhukai@homolo.com>
2023-07-04 22:44:10 +0800

    feat: gpt支持代理; 修复5.2后v进度获取错误的问题

--- src/main/java/com/github/novicezk/midjourney/service/NotifyServiceImpl.java
+++ src/main/java/com/github/novicezk/midjourney/service/NotifyServiceImpl.java
@@ -1,7 +1,9 @@
 package com.github.novicezk.midjourney.service;
 
+import cn.hutool.cache.CacheUtil;
+import cn.hutool.cache.impl.TimedCache;
+import cn.hutool.core.exceptions.CheckedUtil;
 import cn.hutool.core.text.CharSequenceUtil;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.novicezk.midjourney.Constants;
@@ -9,10 +11,6 @@ import com.github.novicezk.midjourney.ProxyProperties;
 import com.github.novicezk.midjourney.enums.TaskStatus;
 import com.github.novicezk.midjourney.support.Task;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -22,16 +20,18 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Slf4j
 @Service
 public class NotifyServiceImpl implements NotifyService {
 	private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 	private final ThreadPoolTaskExecutor executor;
-	private final Map<String, Object> taskLocks = new ConcurrentHashMap<>();
+	private final TimedCache<String, Object> taskLocks = CacheUtil.newTimedCache(Duration.ofHours(1).toMillis());
 
 	public NotifyServiceImpl(ProxyProperties properties) {
 		this.executor = new ThreadPoolTaskExecutor();
-		this.executor.setCorePoolSize(properties.getQueue().getNotifyPoolSize());
+		this.executor.setCorePoolSize(properties.getNotifyPoolSize());
 		this.executor.setThreadNamePrefix("TaskNotify-");
 		this.executor.initialize();
 	}
@@ -42,32 +42,28 @@ public class NotifyServiceImpl implements NotifyService {
 		if (CharSequenceUtil.isBlank(notifyHook)) {
 			return;
 		}
-		// 获取线程所需的参数，避免在线程中获取
-		String tastId = task.getId();
+		String taskId = task.getId();
 		TaskStatus taskStatus = task.getStatus();
-		Object taskLock = taskLocks.computeIfAbsent(tastId, id -> new Object()); // 获取任务对应的锁对象，避免同一任务id进度推送顺序错乱问题
-		log.debug("创建任务变更线程, 任务ID: {}, status: {}", tastId, taskStatus);
+		Object taskLock = this.taskLocks.get(taskId, (CheckedUtil.Func0Rt<Object>) Object::new);
 		try {
 			String paramsStr = OBJECT_MAPPER.writeValueAsString(task);
 			this.executor.execute(() -> {
 				synchronized (taskLock) {
 					try {
-						log.debug("开始推送任务变更, 任务ID: {}, status: {}", tastId, taskStatus);
 						ResponseEntity<String> responseEntity = postJson(notifyHook, paramsStr);
 						if (responseEntity.getStatusCode() == HttpStatus.OK) {
-							log.debug("推送任务变更成功, 任务ID: {}, status: {}", tastId, taskStatus);
+							log.debug("推送任务变更成功, 任务ID: {}, status: {}, notifyHook: {}", taskId, taskStatus, notifyHook);
 						} else {
-							log.warn("推送任务变更失败, 任务ID: {}, code: {}, msg: {}", tastId, responseEntity.getStatusCodeValue(), responseEntity.getBody());
+							log.warn("推送任务变更失败, 任务ID: {}, notifyHook: {}, code: {}, msg: {}", taskId, notifyHook, responseEntity.getStatusCodeValue(), responseEntity.getBody());
 						}
 					} catch (Exception e) {
-						log.warn("推送任务变更失败, 任务ID: {}, 描述: {}", tastId, e.getMessage());
+						log.warn("推送任务变更失败, 任务ID: {}, notifyHook: {}, 描述: {}", taskId, notifyHook, e.getMessage());
 					}
 				}
 			});
 		} catch (JsonProcessingException e) {
-			log.warn("创建任务ID: {}, status: {}, 描述: {}", tastId, taskStatus, e.getMessage());
+			log.warn("推送任务变更失败, 任务ID: {}, notifyHook: {}, 描述: {}", taskId, notifyHook, e.getMessage());
 		}
-		
 	}
 
 	private ResponseEntity<String> postJson(String notifyHook, String paramsJson) {
